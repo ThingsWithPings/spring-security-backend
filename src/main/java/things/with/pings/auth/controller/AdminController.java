decd28fa52c991c1eca002860928b0df1aee8542
viraj-directpay <viraj@directpay.lk>
2023-08-26 12:47:51 +0530

    add user and admin controllers

---
+++ src/main/java/things/with/pings/auth/controller/AdminController.java
@@ -0,0 +1,15 @@
+package things.with.pings.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/hello")
+    public String helloAdminController(){
+        return "Admin level access";
+    }
+}
---
+++ src/main/java/things/with/pings/auth/controller/UserController.java
@@ -0,0 +1,17 @@
+package things.with.pings.auth.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+
+    @GetMapping("/hello")
+    public String helloUserController(){
+        return "User level access";
+    }
+}
